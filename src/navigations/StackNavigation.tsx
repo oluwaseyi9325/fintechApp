90613892f496d11e613969c865772993d07dbaa8
Oluwaseyi9325 <adedokunseyi96@gmail.com>
2024-03-14 12:05:02 +0100

    config the stack.navigator

--- src/navigations/StackNavigation.tsx
+++ src/navigations/StackNavigation.tsx
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import routes from './routes'
+import HomepageScreen from '../screens/HomepageScreen'
 
+const Stack= createStackNavigator()
 export default function StackNavigation() {
   return (
-    <View>
-      <Text>StackNavigation</Text>
-    </View>
+  <Stack.Navigator>
+    <Stack.Screen name={routes.HOMEPAGE} component={HomepageScreen} />
+  </Stack.Navigator>
   )
 }
