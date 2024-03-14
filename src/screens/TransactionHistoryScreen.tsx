860ee6208486ce35b74fc5216952d6c4e71412bc
Oluwaseyi9325 <adedokunseyi96@gmail.com>
2024-03-14 11:59:41 +0100

    rnf transaction screen

---
+++ src/screens/TransactionHistoryScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function TransactionHistoryScreen() {
+  return (
+    <View>
+      <Text>TransactionHistoryScreen</Text>
+    </View>
+  )
+}
