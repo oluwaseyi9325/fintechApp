f62e938688e004d9418b0a8fb7ac0a32a6ccb303
Oluwaseyi9325 <adedokunseyi96@gmail.com>
2024-03-14 12:08:31 +0100

    finish the stack config for all screen

--- src/navigations/StackNavigation.tsx
+++ src/navigations/StackNavigation.tsx
@@ -3,12 +3,26 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import routes from './routes'
 import HomepageScreen from '../screens/HomepageScreen'
+import ConfirmationScreen from '../screens/ConfirmationScreen'
+import TransferScreen from '../screens/TransferScreen'
+import TransactionHistoryScreen from '../screens/TransactionHistoryScreen'
+import CardScreen from '../screens/CardScreen'
+import NotificationScreen from '../screens/NotificationScreen'
+import ProfileCompletedScreen from '../screens/ProfileCompletedScreen'
+import ProfileScreen from '../screens/ProfileScreen'
 
 const Stack= createStackNavigator()
 export default function StackNavigation() {
   return (
   <Stack.Navigator>
     <Stack.Screen name={routes.HOMEPAGE} component={HomepageScreen} />
+    <Stack.Screen name={routes.TRANSFER_SCREEN} component={TransferScreen} />
+    <Stack.Screen name={routes.CONFIRMATION_SCREEN} component={ConfirmationScreen} />
+    <Stack.Screen name={routes.TRANSACTION_HISTORY_SCREEN} component={TransactionHistoryScreen} />
+    <Stack.Screen name={routes.CARD_SCREEN} component={CardScreen} />
+    <Stack.Screen name={routes.NOTIFICATION_SCREEN} component={NotificationScreen} />
+    <Stack.Screen name={routes.PROFILE_COMPLETED_SCREEN} component={ProfileCompletedScreen} />
+    <Stack.Screen name={routes.PROFILE_SCREEN} component={ProfileScreen} />
   </Stack.Navigator>
   )
 }
